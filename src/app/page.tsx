7468ee7067551eadf6c9b0f8cde199aa8dbe6437
Shan Kuriakose <shankuriakose@Shans-MacBook-Air.local>
2025-04-27 00:45:42 +0530

    intial_chores: file structure

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Dashboard } from "@/components/Dashboard/Dashboard";
+import { Sidebar } from "@/components/Sidebar/Sidebar";
+
+export default function  Home(){
+  return <main className="">
+    <Sidebar />
+    <Dashboard />
+  </main>
+}
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,7 @@
+import react from 'react'
+
+export const Sidebar = () => {
+    return (
+        <div>SideBar</div>
+    )
+}
